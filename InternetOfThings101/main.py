0253a55a1980242b0399d7cd2020c4732ed316d2
Erick Cardona <cielosdecristal@gmail.com>
2016-04-23 16:00:33 +0000

    23/04/2016 a Signed-off-by: Erick Cardona <cielosdecristal@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,15 +1,36 @@
-import pyupm_i2clcd as lcd
+#!/usr/bin/python
 
-# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS) 
-myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+import psutil
+import signal
+import sys
+import time
 
-myLcd.setCursor(0,0)
-# RGB Blue
-#myLcd.setColor(53, 39, 249)
+from threading import Thread
 
-# RGB Red
-myLcd.setColor(255, 0, 0)
+def interruptHandler(signal, frame):
+    sys.exit(0)
 
-myLcd.write('Hello World')
-myLcd.setCursor(1,2)
-myLcd.write('Hello World')
+def dataNetwork():
+    netdata = psutil.net_io_counters()
+    return netdata.packets_sent + netdata.packets_recv
+
+def dataNetworkHandler():
+    idDevice = "IoT101Device"
+    while True:
+        packets = dataNetwork()
+        message = idDevice + " " + str(packets)
+        print "dataNetworkHandler " + message
+        time.sleep(1)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, interruptHandler)
+
+    threadx = Thread(target=dataNetworkHandler)
+    threadx.start()
+
+    while True:
+        print "Hello Internet of Things 101"
+        time.sleep(5)
+
+# End of File
