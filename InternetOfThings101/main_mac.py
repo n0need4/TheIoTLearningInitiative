9ce6ee8bbc4f7aefc0097a3fdc79a45a94b93823
Erick Cardona <cielosdecristal@gmail.com>
2016-04-23 17:40:04 +0000

    x
    Signed-off-by: Erick Cardona <cielosdecristal@gmail.com>

--- InternetOfThings101/main_mac.py
+++ InternetOfThings101/main_mac.py
@@ -2,7 +2,6 @@
 
 import paho.mqtt.client as paho
 import psutil
-import pywapi
 import signal
 import sys
 import time
@@ -19,37 +18,21 @@ def on_publish(mosq, obj, msg):
 def dataNetwork():
     netdata = psutil.net_io_counters()
     return netdata.packets_sent + netdata.packets_recv
+
 from uuid import getnode as get_mac
 
 def dataNetworkHandler():
-    idDevice = str(get_mac)
+    mac = str(get_mac)
+    idDevice = mac
     mqttclient = paho.Client()
     mqttclient.on_publish = on_publish
     mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
         packets = dataNetwork()
         message = idDevice + " " + str(packets)
-        print "dataNetworkHandler " + message
+        print "MQTT dataNetworkHandler " + message
         mqttclient.publish("IoT101/Network", message)
         time.sleep(1)
-
-def on_message(mosq, obj, msg):
-    print "dataMessageHandler %s %s" % (msg.topic, msg.payload)
-
-def dataMessageHandler():
-    mqttclient = paho.Client()
-    mqttclient.on_message = on_message
-    mqttclient.connect("test.mosquitto.org", 1883, 60)
-    mqttclient.subscribe("IoT101/Message", 0)
-    while mqttclient.loop() == 0:
-        pass
-
-def dataWeatherHandler():
-    weather = pywapi.get_weather_from_yahoo('MXJO0043', 'metric')
-    message = "Weather Report in " + weather['location']['city']
-    message = message + ", Temperature " + weather['condition']['temp'] + " C"
-    message = message + ", Atmospheric Pressure " + weather['atmosphere']['pressure'] + " mbar"
-    print message
 
 if __name__ == '__main__':
 
@@ -58,13 +41,9 @@ if __name__ == '__main__':
     threadx = Thread(target=dataNetworkHandler)
     threadx.start()
 
-    threadx = Thread(target=dataMessageHandler)
-    threadx.start()
-
-    dataWeatherHandler()
-
     while True:
         print "Hello Internet of Things 101"
         time.sleep(5)
+
 
 # End of File
